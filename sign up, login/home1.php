52c05415ce187754111daf2d081f216402d4c121
Ravindua-Wijekoon <ravinduwijekoon123@gmail.com>
2023-11-10 15:48:31 +0530

    update007

---
+++ sign up, login/home1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+
+    <link rel="stylesheet" href="../lib/style/home.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Sofia+Sans:wght@200&display=swap"
+        rel="stylesheet" />
+</head>
+
+<body>
+    <div class="section-01">
+        <div class="div1">
+            <img class="logo" src="../images/logo.png" alt="" />
+            <div class="div2">
+                <div class="home">Home</div>
+                <div>About Us</div>
+                <div>Login</div>
+                <img src="../images/search.png" height="20px" alt="" />
+            </div>
+        </div>
+        <div class="welcome-txt">
+            <div>Welcome to <span>Tars</span></div>
+        </div>
+    </div>
+    <div class="section-02"></div>
+    <div class="section-03"></div>
+</body>
+
+</html>
