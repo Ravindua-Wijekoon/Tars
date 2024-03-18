204c7534623243e711f83d242a2534ebe9be16fe
Ravindua-Wijekoon <ravinduwijekoon123@gmail.com>
2024-03-18 19:09:12 +0530

    update

---
+++ admin/admin_dashboard.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin Dashboard</title>
+    <!-- ======= Styles ====== -->
+    <link rel="stylesheet" href="../lib/style/admin_dash.css" />
+</head>
+
+<body>
+    <!-- =============== Left ================ -->
+    <div class="container">
+        <div class="left-box">
+            <img src="../images/logo01.png" alt="">
+        </div>
+
+        <!-- ========================= Main ==================== -->
+        <div class="main">
+            <div class="topbar">
+                <div class="messages">
+                    <img src="../images/messages.png" alt="" />
+                </div>
+                <div class="notification">
+                    <img src="../images/notification.png" alt="" />
+                </div>
+                <div class="logout">
+                    <a class="none" href="#">Logout</a>
+                </div>
+                <div class="user">
+                    <img src="../images/profile_photos/customer02.jpg" alt="" />
+                </div>
+            </div>
+
+            <!-- ======================= Cards ================== -->
+            <div class="cardBox">
+                <div class="card">
+                    <div>
+                        <div class="cardName">Students</div>
+                        <div class="numbers">05</div>
+                    </div>
+
+                    <div>
+                        <img src="../images/icon _user_.png" alt="">
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <div class="cardName">Approved <br>Companies</div>
+                        <div class="numbers">05</div>
+                    </div>
+
+                    <div>
+                        <img src="../images/icon _tick circle_.png" alt="">
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <div class="cardName">Request <br>Approvals</div>
+                        <div class="numbers">03</div>
+                    </div>
+
+                    <div>
+                        <img src="../images/briefcase.png" alt="">
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <div class="cardName">Notices</div>
+                        <div class="numbers">08</div>
+                    </div>
+
+                    <div>
+                        <img src="../images/messagetext1.png" alt="">
+                    </div>
+                </div>
+                <div class="bt-height"></div>
+            </div>
+
+            <!-- =========== Scripts =========
+            <script src="assets/js/main.js"></script> -->
+
+        </div>
+    </div>
+</body>
+
+</html>
