38e014850b6a8c4bf07b494d167b203998809481
chamod961 <2021t01252@stu.cmb.ac.lk>
2024-03-24 19:54:02 +0530

    comit

---
+++ database/tars_db_correct.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2024 at 03:09 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tars_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education_info`
+--
+
+CREATE TABLE `education_info` (
+  `id` int(100) NOT NULL,
+  `institution` varchar(255) NOT NULL,
+  `sdate` varchar(255) NOT NULL,
+  `edate` varchar(255) NOT NULL,
+  `std_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `experience_info`
+--
+
+CREATE TABLE `experience_info` (
+  `id` int(100) NOT NULL,
+  `experience` varchar(255) NOT NULL,
+  `sdate` varchar(255) NOT NULL,
+  `edate` varchar(255) NOT NULL,
+  `std_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `language_info`
+--
+
+CREATE TABLE `language_info` (
+  `id` int(100) NOT NULL,
+  `language` varchar(255) NOT NULL,
+  `std_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skill_info`
+--
+
+CREATE TABLE `skill_info` (
+  `id` int(100) NOT NULL,
+  `skill` varchar(255) NOT NULL,
+  `std_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_info`
+--
+
+CREATE TABLE `student_info` (
+  `id` int(255) NOT NULL,
+  `uocindex` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `nameinitials` varchar(255) NOT NULL,
+  `bio` varchar(255) NOT NULL,
+  `dob` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `zipcode` int(100) NOT NULL,
+  `github` varchar(255) NOT NULL,
+  `linkedin` varchar(255) NOT NULL,
+  `portfolio` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `education_info`
+--
+ALTER TABLE `education_info`
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `experience_info`
+--
+ALTER TABLE `experience_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `language_info`
+--
+ALTER TABLE `language_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `skill_info`
+--
+ALTER TABLE `skill_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_info`
+--
+ALTER TABLE `student_info`
+  ADD PRIMARY KEY (`uocindex`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `education_info`
+--
+ALTER TABLE `education_info`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `experience_info`
+--
+ALTER TABLE `experience_info`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `language_info`
+--
+ALTER TABLE `language_info`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `skill_info`
+--
+ALTER TABLE `skill_info`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `student_info`
+--
+ALTER TABLE `student_info`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
